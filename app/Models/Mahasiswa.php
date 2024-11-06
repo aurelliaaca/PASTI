3718726fcba2bcab78cfe9048b8f699f28dab291
revallne <dhelarevaline@gmail.com>
2024-11-06 22:24:19 +0700

    benerin database

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'mahasiswa';
+    protected $primaryKey = 'nim';
+    public $incrementing = false; // Because 'nim' is not auto-incrementing
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'nama',
+        'nim',
+        'smt',
+        'telp',
+        'email',
+        'alamat',
+        'dosenwali'
+    ];
+}
